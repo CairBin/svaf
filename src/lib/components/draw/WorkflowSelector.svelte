f71cd2030fce03b91bcf1292f092df14426100c3
二叉树树 <acofork@qq.com>
2026-05-24 07:20:56 +0800

    fix: .txt 工作流显示时不带扩展名

--- src/lib/components/draw/WorkflowSelector.svelte
+++ src/lib/components/draw/WorkflowSelector.svelte
@@ -160,7 +160,7 @@
 								{#if loadingPath === wf.path}
 									<Icon icon="mdi:loading" class="size-4 shrink-0 animate-spin" />
 								{/if}
-								<span class="truncate">{wf.path.split('/').pop()?.replace('.json', '')}</span>
+								<span class="truncate">{wf.path.split('/').pop()?.replace(/\.(json|txt)$/, '')}</span>
 							</button>
 						{/each}
 					</div>
